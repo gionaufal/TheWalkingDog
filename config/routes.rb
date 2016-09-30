02ceba706882fd9ce4c56b5191182be1a75a06dd
Giovanni <g10v45+github@gmail.com>
2016-09-29 22:05:06 -0300

    user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'home#index'
   resources :walkers, only: [:show]
   get '/search/', to: 'walkers#search'
---
+++ spec/features/user_sign_up_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+feature 'user sign up' do
+  scenario 'successfully' do
+    user = build(:user)
+
+    visit root_path
+
+    click_on 'Cadastrar'
+
+    fill_in 'Nome', with: user.name
+    fill_in 'Endereço', with: user.address
+    fill_in 'CPF', with: user.cpf
+    fill_in 'Email', with: user.email
+    fill_in 'Telefone', with: user.phone
+    fill_in 'Senha', with: user.password
+    fill_in 'Confirme sua senha', with: user.password
+
+    within('form') do
+      click_on 'Cadastrar'
+    end
+
+    expect(page).to have_content 'Cadastro efetuado com sucesso!'
+    expect(current_path).to eq root_path
+  end
+  scenario 'cant sign up if miss some fields' do
+    user = build(:user)
+
+    visit root_path
+
+    click_on 'Cadastrar'
+
+    fill_in 'Nome', with: user.name
+    fill_in 'CPF', with: user.cpf
+    fill_in 'Telefone', with: user.phone
+
+
+    within('form') do
+      click_on 'Cadastrar'
+    end
+
+    expect(page).to have_content 'não pode ficar em branco'
+  end
+end

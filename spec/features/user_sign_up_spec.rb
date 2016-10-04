5da520dc80d6b08a06973aef13ddce6f766070f0
Giovanni <g10v45+github@gmail.com>
2016-10-03 22:02:59 -0300

    fix user params in devise

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -7,7 +7,6 @@ feature 'user sign up' do
     visit root_path
 
     click_on 'Cadastrar'
-
     fill_in 'Nome', with: user.name
     fill_in 'Endereço', with: user.address
     fill_in 'CPF', with: user.cpf
@@ -21,6 +20,7 @@ feature 'user sign up' do
     end
 
     expect(page).to have_content 'Cadastro efetuado com sucesso!'
+    expect(page).to have_content "Olá, #{user.name}"
     expect(current_path).to eq root_path
   end
   scenario 'cant sign up if miss some fields' do

fc172f4a07fd7ee981922b752d98f1a49b059293
henrique e thiago <pin.henrique03@gmail.com>
2016-10-05 22:18:29 -0300

    defining home styles [wip]

--- app/assets/javascripts/components/autocomplete.js
+++ app/assets/javascripts/components/autocomplete.js
@@ -10,15 +10,20 @@ $(document).ready(function() {
         text: $(item).text()
       }
     });
+    var select = autocomplete.find('select');
     var input = autocomplete.find('input[type=submit]');
 
     var button = '<div class="autocomplete__button button primary white">';
-    button += '<input class="autocomplete__input" type="text" />';
+    button += '<input class="autocomplete__input" type="text" value="' + label + '" />';
     button += '</div>';
 
     var foundTemplate = function(array) {
-      var list = '<div class="autocomplete__list">';
-      array.map()
+      var list = '<ul class="autocomplete__list">';
+      array.map(function(item) {
+        list += '<li class="item">' + item + '</li>';
+      });
+      list += '</ul>';
+      return list;
     };
 
     autocomplete.append(button);
@@ -26,18 +31,49 @@ $(document).ready(function() {
     var activeButton = autocomplete.find('.autocomplete__button');
     var activeInput = activeButton.find('input');
 
+    function addItem (list, item) {
+      if (list.indexOf(item) === -1) {
+        list.push(item);
+      }
+    };
+
     activeInput.on('keyup', function(e) {
       var found = [];
       var value = activeInput.val();
+      activeButton.find('.autocomplete__list').remove();
       function search(element) {
         return element.text.indexOf(value) === 0 ? element : undefined;
       }
-      if (value.length > 0) {
-        found.push(items.find(search));
-
+      if (value.length > 0 && items.find(search)) {
+        addItem(found, items.find(search).text)
+        activeButton.append(foundTemplate(found));
+      } else {
+        found.push('região não encontrada');
+        activeButton.append(foundTemplate(found));
       }
-      console.log(found)
     });
+
+    activeInput.on('click', function() {
+      $(this).val('');
+    });
+
+    activeButton.on('click', '.item', function(item) {
+      var text = $(item.currentTarget).text();
+
+      if (text) {
+        activeInput.val(text);
+        activeButton.find('.autocomplete__list').remove();
+        select.val(text);
+        setTimeout(function() {
+          input.trigger('click');
+        }, 300);
+      }
+    });
+
+    // $('body').on('click', function(target) {
+    //   if ($(target.currentTarget).hasClass('.overlay')) {
+    //     activeInput.val(label);
+    //   }
+    // });
   });
-  console.log(autocompletes);
 });
